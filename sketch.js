9ee729e3cb9934dcc8434b68c61a4ebe1e10f0d0
roelshr <125639844+roelshr@users.noreply.github.com>
2023-05-07 10:27:11 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 
 function setup() {
   createCanvas(400,400);
-
+  S1 = new Student("bob", 6, 1)
+  S1.display()
 }
 
 function draw() 
--- student.js
+++ student.js
@@ -1,8 +1,13 @@
 
-class Student
-{
-   
+class Student {
+    constructor(name, age, grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    display() {
+        console.log(this.name)
+        console.log(this.age)
+        console.log(this.grade)
+    }
 }
-
-
-
